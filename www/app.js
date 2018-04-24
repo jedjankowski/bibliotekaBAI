d3790b5c45c7a71a929ca2410eea89a745f06cfa
jedrek333 <jedrekjjj3@gmail.com>
2018-04-24 18:19:56 +0200

    logowanie

---
+++ www/app.js
@@ -0,0 +1,59 @@
+(function() {
+    //  Initialize Firebase
+    const config = {
+        apiKey: "AIzaSyDR2IJ3Ea-4p2I1BQs7AuJDs0EjCEE4IAM",
+        authDomain: "baiprojekt-54e2b.firebaseapp.com",
+        databaseURL: "https://baiprojekt-54e2b.firebaseio.com",
+        projectId: "baiprojekt-54e2b",
+        storageBucket: "baiprojekt-54e2b.appspot.com",
+        messagingSenderId: "726560257409"
+    };
+    firebase.initializeApp(config);
+
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnSignUp = document.getElementById('btnSignUp');
+    const btnLogout = document.getElementById('btnLogout');
+
+    //login event
+    btnLogin.addEventListener('click', e => {
+        //pobieranie maila i hasla
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+
+        //zaloguj
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+    });
+
+    //rejestracja
+    //do zrobnienia: sprawdzanie czy mail jest poprawnie wpisany (wzor@mail.com)
+    btnSignUp.addEventListener('click', e => {
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+
+        //zaloguj
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise
+            .catch(e => console.log(e.message));
+    } );
+
+    btnLogout.addEventListener('click', ev => {
+        firebase.auth().signOut();
+    });
+
+    //listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log(firebaseUser);
+            btnLogout.classList.remove('hide');
+        } else {
+            console.log('NIEZAREJESTROWANY');
+            btnLogout.classList.add('hide');
+        }
+    });
+
+}());
